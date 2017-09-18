c1dd805a1f1f50e394f56799b41a4634d85b75c9
johson <15201296417@163.com>
2017-09-18 17:36:39 +0800

    斐波那契数列

---
+++ demo.php
@@ -0,0 +1,33 @@
+<?php
+function sum($n)
+{
+    if ($n < 3) {
+        return 1;
+    } else {
+        return sum($n-1) + sum($n-2);
+    }
+}
+
+function niubi($n) {
+    if ($n < 3) {
+        return 1;
+    } else {
+        $a = 1;
+        $b = 1;
+        for ($m = 3; $m <= $n; $m++) {
+            $tmp = $a + $b;
+            //准备下一级的前数
+            $a = $b;
+            //准备下一级的后数
+            $b = $tmp;
+        }
+        return $tmp;
+    }
+}
+
+
+
+
+
+echo sum(19);echo "\n\n";
+echo sum(19);
